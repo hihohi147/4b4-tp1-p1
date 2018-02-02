4fa602ec52ab15a77e3082ca9a68afed13ec376c
Hamza Ouchrif <maradona_hamza@hotmail.com>
2018-02-02 11:06:26 -0500

    Ajout du constructeur et des attributs

--- CV.java
+++ CV.java
@@ -2,7 +2,20 @@ import java.util.Arrays;
 
 public class CV {
 
+public static String nom, prenom, formation, attentes;
+	public static int experienceTravail;
+	public static String[] competences;
 
+	public CV(String nom, String prenom, String formation, String attentes, int experienceTravail,
+			String[] competences) {
+		CV.nom = nom;
+		CV.prenom = prenom;
+		CV.formation = formation;
+		CV.attentes = attentes;
+		CV.experienceTravail = experienceTravail;
+		CV.competences = competences;
+
+	}
 
 
 	public static void main(String[] args) {
